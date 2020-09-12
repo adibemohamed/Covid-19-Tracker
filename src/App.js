ce0dd3fa10e07f03e858f63cf30119280f724fdc
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-12 23:57:37 +0000

    ADD dropdown for worlwide

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'; 
 import './App.css';
+import {FormControl, Select} from '@material-ui'
 
 function App() {
   return (
     <div className="App">
-    <h1>Starting</h1>
+    <h1>COVID-19 Tracker</h1>
+    <FormControl className="app__dropdown">
+      <Select 
+      variant="outline"
+      value="abc">
+        <MenuItem value="worlwide">Worlwide</MenuItem>
+      </Select>
+    </FormControl>
     </div>
   );
 }
