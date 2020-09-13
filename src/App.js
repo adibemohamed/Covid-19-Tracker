2095094b9f37bc824ac9e3c1864bcab24134258e
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-13 22:02:48 +0000

    ADD countries dropdown

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from 'react'; 
-import './App.css';
-import {FormControl, Select} from '@material-ui'
+import React, { useState } from "react";
+import "./App.css";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
 
 function App() {
+  const [countries, setCountries] = useState(['USA', 'UK', 'INDIA']);
+
+
   return (
-    <div className="App">
-    <h1>COVID-19 Tracker</h1>
-    <FormControl className="app__dropdown">
-      <Select 
-      variant="outline"
-      value="abc">
-        <MenuItem value="worlwide">Worlwide</MenuItem>
-      </Select>
-    </FormControl>
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {/* Loop throught all the countries and show dropdown */}
+            {
+              countries.map(country => (
+                <MenuItem value="worlwide">{country}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
