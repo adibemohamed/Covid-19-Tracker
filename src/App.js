0ccec87613f75f9c82dfae5168083e9656674e5d
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-14 02:06:29 +0000

    ADD stats Circles to leaflet map

--- src/App.js
+++ src/App.js
@@ -15,12 +15,13 @@ import LineGraph from "./LineGraph";
 import "leaflet/dist/leaflet.css";
 
 function App() {
-  const [countries, setCountries] = useState([""]);
+  const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({});
   const [tableData, setTableData] = useState([]);
   const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
   const [mapZoom, setMapZoom] = useState(3);
+  const [mapCountries, setMapCountries] = useState([]);
 
   // Bring stats of all countries
   useEffect(() => {
@@ -46,6 +47,7 @@ function App() {
 
           setTableData(sortedData);
           setCountries(countries);
+          setMapCountries(data);
         });
     };
 
@@ -68,6 +70,9 @@ function App() {
 
         setCountry(countryCode);
         setCountryInfo(data);
+
+        setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+        setMapZoom(4);
       });
   };
 
@@ -112,7 +117,7 @@ function App() {
           />
         </div>
 
-        <Map center={mapCenter} zoom={mapZoom} />
+        <Map countries={mapCountries} center={mapCenter} zoom={mapZoom} />
       </div>
 
       <Card className="app__right">
--- src/util.js
+++ src/util.js
@@ -1,3 +1,22 @@
+import React from "react";
+import numeral from "numeral";
+import { Circle, Popup } from "react-leaflet";
+
+const casesTypeColors = {
+  cases: {
+    hex: "#CC1034",
+    multiplier: 800,
+  },
+  recovered: {
+    hex: "#7dd71d",
+    multiplier: 1200,
+  },
+  deaths: {
+    hex: "#fb4443",
+    multiplier: 2000,
+  },
+};
+
 export const sortData = (data) => {
   const sortedData = [...data];
 
@@ -24,3 +43,20 @@ export const sortData = (data) => {
 // [11,2,22,1].sort((a, b) => a - b)
 
 // Sorts an array.
+
+// Draw circles on the map with interactive tooltip
+export const showDataOnMap = (data, casesType = "cases") =>
+  data.map((country) => (
+    <Circle
+      center={[country.countryInfo.lat, country.countryInfo.long]}
+      fillOpacity={0.4}
+      color={casesTypeColors[casesType].hex}
+      radius={
+        Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+      }
+    >
+      <Popup>
+        <h1>I A Popup</h1>
+      </Popup>
+    </Circle>
+  ));
